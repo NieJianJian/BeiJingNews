ebc9826d03ed3a1f5d92f6dc71a1ff4279c2afa7
NieJianJian <niejianjian_sir@163.com>
2016-08-04 16:22:20 +0800

    修改butterknife版本，8以上不能用

--- BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/activity/SplashActivity.java
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/activity/SplashActivity.java
@@ -5,21 +5,23 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import beijingnews.njj.com.beijingnews.R;
-import butterknife.BindView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends BaseActivity {
 
-    @BindView(R.id.rl_splash_view)
+    @Bind(R.id.rl_splash_view)
     RelativeLayout rl;
-    @BindView(R.id.iv)
+    @Bind(R.id.iv)
     ImageView mImageView;
-//    http://blog.csdn.net/departure_/article/details/51595286
+
+    //    http://blog.csdn.net/departure_/article/details/51595286
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
         mImageView.setImageResource(R.drawable.splash_bg_newyear);
 
