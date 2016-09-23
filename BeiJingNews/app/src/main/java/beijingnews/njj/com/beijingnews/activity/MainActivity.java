7d18fa2a584103837c5b130d1817dae2c65e9505
NieJianJian <niejianjian_sir@163.com>
2016-09-23 08:10:57 +0800

    侧滑菜单的相关属性设置

--- BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/activity/MainActivity.java
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/activity/MainActivity.java
@@ -2,13 +2,45 @@ package beijingnews.njj.com.beijingnews.activity;
 
 import android.os.Bundle;
 
-import beijingnews.njj.com.beijingnews.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends BaseActivity {
+import beijingnews.njj.com.beijingnews.R;
+import beijingnews.njj.com.beijingnews.utils.DensityUtil;
+
+
+/**
+ * 1.导入SlidingMenu库
+ * 2.工程关联SlidingMenu库
+ * 3.把Activity继承SlidingFragmentActivity
+ * 4.把onCreate改成public修饰
+ * 5.设置SlideMenu细节
+ * 5.1 设置左侧页面
+ * 5.2 设置右侧页面
+ * 5.3 设置主页面
+ * 5.4 设置模式：主页面 + 左侧页面 ; 主页面 + 右侧页面 ; 左侧页面 + 主页面 + 右侧页面
+ * 5.5 设置滑动模式：全屏，边缘，不可滑动
+ * 5.6 设置页面占比宽度
+ */
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置主页面
         setContentView(R.layout.activity_main);
+        // 设置左侧页面
+        setBehindContentView(R.layout.left_menu);
+        // 得到SlidingMenu
+        SlidingMenu slidingMenu = getSlidingMenu();
+        // 设置右侧菜单
+//        slidingMenu.setSecondaryMenu(R.layout.right_menu);
+        // 设置模式
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        // 设置滑动模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        // 设置页面占比宽度
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(this, 200));
+
     }
 }
