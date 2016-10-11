ed3641f96f4d2b4f30c1cf84283c67069f34b740
NieJianJian <niejianjian_sir@163.com>
2016-10-12 00:24:16 +0800

    设置title的menu按钮

--- BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/pager/NewsCenterPager.java
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/pager/NewsCenterPager.java
@@ -3,6 +3,7 @@ package beijingnews.njj.com.beijingnews.pager;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
 import beijingnews.njj.com.beijingnews.base.BasePager;
@@ -22,6 +23,7 @@ public class NewsCenterPager extends BasePager {
         super.initData();
         // 设置标题
         tv_title.setText("新闻中心");
+        ib_menu.setVisibility(View.VISIBLE);
 
         // 设置内容
         TextView textView = new TextView(mActivity);
