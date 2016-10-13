6be9dc2644dca754c1124258bd75ef3462100304
NieJianJian <niejianjian_sir@163.com>
2016-10-13 23:21:16 +0800

    增加常量类，存放接口

---
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/utils/ConstantUtils.java
@@ -0,0 +1,13 @@
+package beijingnews.njj.com.beijingnews.utils;
+
+/**
+ * Created by Administrator on 2016/10/12.
+ */
+public class ConstantUtils {
+
+    public static final String base_url = "http://192.168.16.108:8080/zhbj";
+
+    public static final String newscenter_url = base_url + "/categories.json";
+
+
+}
