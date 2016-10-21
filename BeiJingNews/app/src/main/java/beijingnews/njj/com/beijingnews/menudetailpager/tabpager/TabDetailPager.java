387ae128786851bf5124df59fe60c015cb7b2832
NieJianJian <niejianjian_sir@163.com>
2016-10-21 14:12:01 +0800

    搭建菜单新闻详情的布局

--- BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/menudetailpager/tabpager/TabDetailPager.java
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/menudetailpager/tabpager/TabDetailPager.java
@@ -1,12 +1,17 @@
 package beijingnews.njj.com.beijingnews.menudetailpager.tabpager;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import beijingnews.njj.com.beijingnews.R;
 import beijingnews.njj.com.beijingnews.base.MenuDetailBasePager;
 import beijingnews.njj.com.beijingnews.domain.NewsCenterPagerBean;
 
@@ -16,8 +21,17 @@ import beijingnews.njj.com.beijingnews.domain.NewsCenterPagerBean;
  */
 public class TabDetailPager extends MenuDetailBasePager {
 
+    @ViewInject(R.id.viewpager_tabdetail)
+    private ViewPager mViewPager_TabDetail;
+    @ViewInject(R.id.tv_tabledetail)
+    private TextView mTextView_TabDetail;
+    @ViewInject(R.id.listview_tabdetail)
+    private ListView mListView_TabDetail;
+    @ViewInject(R.id.ll_poing_tabledetail)
+    private LinearLayout mLl_Poing_TabDetail;
+
     private final NewsCenterPagerBean.DataBean.ChildrenBean mChildrenBean;
-    TextView textView;
+//    TextView textView;
 
     public TabDetailPager(Activity activity, NewsCenterPagerBean.DataBean.ChildrenBean childrenBean) {
         super(activity);
@@ -26,18 +40,20 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     @Override
     public View initView() {
-        Log.i("niejianjian"," -> TabDetailPager -> initView");
-        textView = new TextView(mActivity);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.i("niejianjian", " -> TabDetailPager -> initView");
+        View view = View.inflate(mActivity, R.layout.tabdetail_pager, null);
+        x.view().inject(this, view);
+//        textView = new TextView(mActivity);
+//        textView.setTextSize(30);
+//        textView.setGravity(Gravity.CENTER);
+//        textView.setTextColor(Color.RED);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText(mChildrenBean.getTitle());
+//        textView.setText(mChildrenBean.getTitle());
         Log.i("niejianjian", " -> TabDetailPager -> initData() -> " + mChildrenBean.getTitle());
     }
 }
