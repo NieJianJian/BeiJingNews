3517967b18c9c21e46609bc9eb7876bf74565f35
NieJianJian <niejianjian_sir@163.com>
2016-10-24 20:16:32 +0800

    处理内外两层viewpager的滑动关系，利用坐标偏移量来处理

--- BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/view/HorizontalViewPager.java
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/view/HorizontalViewPager.java
@@ -18,10 +18,48 @@ public class HorizontalViewPager extends ViewPager {
         super(context, attrs);
     }
 
+    private float startX, startY;
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // 1.记录其实位置
+                startX = ev.getX();
+                startY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 2.获得最新坐标
+                float endX = ev.getX();
+                float endY = ev.getY();
+                // 3.计算偏移量
+                float distanceX = endX - startX;
+                float distanceY = endY - startY;
+                // 4.判断方向
+                if (Math.abs(distanceX) > Math.abs(distanceY)) {
+                    // 水平方向滑动
+                    if (getCurrentItem() == 0 && distanceX > 0) {
+                        getParent().requestDisallowInterceptTouchEvent(false);
+                    } else if ((getCurrentItem() == getAdapter().getCount() - 1) && distanceX < 0) {
+                        getParent().requestDisallowInterceptTouchEvent(false);
+                    } else {
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                    }
+                } else {
+                    // 竖直方向滑动
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+            default:
+
+                break;
+        }
         // 要父层view把事件传递给自己（让父亲的拦截事件不生效）
-        getParent().requestDisallowInterceptTouchEvent(true); // 禁用拦截
+//        getParent().requestDisallowInterceptTouchEvent(true); // 禁用拦截
         return super.dispatchTouchEvent(ev);
     }
 }
