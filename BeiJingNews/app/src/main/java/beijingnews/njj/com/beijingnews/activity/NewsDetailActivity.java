f4a114e9b2772ded53a68d4137e28b8502e84ec0
NieJianJian <niejianjian_sir@163.com>
2016-10-26 22:49:44 +0800

    新建新闻详情页面，完善titlebar的布局

---
+++ BeiJingNews/app/src/main/java/beijingnews/njj/com/beijingnews/activity/NewsDetailActivity.java
@@ -0,0 +1,18 @@
+package beijingnews.njj.com.beijingnews.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import beijingnews.njj.com.beijingnews.R;
+
+/**
+ * Created by Administrator on 2016/10/26.
+ */
+public class NewsDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
