21738664a30cbd52ad92d0854736af26011dfca7
NieJianJian <niejianjian_sir@163.com>
2016-11-05 22:54:48 +0800

    drwaerlayoutdemo

---
+++ BeiJingNews/drawerLayoutDemo/src/main/java/com/jacksen/drawerlayout/demo3/ThirdDemoActivity.java
@@ -0,0 +1,16 @@
+package com.jacksen.drawerlayout.demo3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jacksen.drawerlayout.R;
+
+public class ThirdDemoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_third);
+	}
+
+}
